2dd60d6716f647e1c827bda592c0ab13c3d5e3f5
apavlov <andrij0pawlov@gmail.com>
2019-11-16 14:48:19 +0200

    build

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Checking required libraries..."
+
+echo "SDL installing..."
+brew install sdl2
+brew install sdl2_image
+echo "SDL installed..."
+
+echo "SFML installing..."
+brew install sfml
+echo "SFML installed..."
+
+echo "OpenGL installing..."
+brew install glew
+echo "OpenGL installed..."
+
+# libOpenGL         GLUT  OpenGL -lSDL2
+## libSDL           -fPIC -lSDL2 -lSDL2_image -lSDL2_ttf
+## libSFML         -fPIC -lsfml-graphics -lsfml-window -lsfml-system -rpath
+## libSFMLSound     -lsfml-audio -lsfml-system
